26730279a58e1349dc8dafa46fa4af86ddf1341c
Kahlil Enmond <slope22@hotmail.com>
2020-08-28 14:25:41 -0400

    fixed up code so that it works

--- src/App.js
+++ src/App.js
@@ -5,14 +5,41 @@ import ScoreBoard from './Components/scoreboard/ScoreBoard';
 import React from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: './assets/images/raccoon.png'
+  }
+
+  const squirrels = {
+    name: 'Sheridan Squirrels',
+    logoSrc: './assets/images/squirrel.png'
+  }
+
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: './assets/images/bunny.png'
+  }
+
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: './assets/images/hound.png'
+  }
+
   return (
     <div className="App">
-      <Game />
-      <Team />
-      <ScoreBoard />
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/Components/scoreboard/Scoreboard.js
+++ src/Components/scoreboard/Scoreboard.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 
-function Scoreboard(props) {
+function ScoreBoard(props) {
     return (
-        <div className="ScoreBoard">
-            <div className="teamStats">
-                <h3>VISITORS</h3>
-                <h3>{props.visitingTeamStats.score}</h3>
-            </div>
-
-            <h3>SCOREBOARD</h3>
-
-            <div className="teamStats">
-                <h3>HOME</h3>
-                <h3>{props.homeTeamStats.score}</h3>
-            </div>
+      <div className="ScoreBoard">
+        <div className="teamStats">
+          <h3>VISITORS</h3>
+          <h3>{props.visitingTeamStats.score}</h3>
         </div>
+  
+        <h3>SCOREBOARD</h3>
+  
+        <div className="teamStats">
+          <h3>HOME</h3>
+          <h3>{props.homeTeamStats.score}</h3>
+        </div>
+      </div>
     )
 }
-export default Scoreboard;
+export default ScoreBoard;
